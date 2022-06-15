657a4274db0798bdc29f6bc5a5ea5985175559d1
hana-zf <100548128+hana-zf@users.noreply.github.com>
2022-06-15 13:01:08 +0800

    ready to try deploying

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 //to disable env content from being shown in production
 if(process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+   require('dotenv').config();
 }
+
+
 
 //console.log(process.env.SECRET);
 //console.log(process.env.API_KEY);
@@ -18,14 +20,21 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const helmet = require('helmet');
+const MongoStore = require('connect-mongo');
+
+
+const mongoSanitize = require('express-mongo-sanitize');
 
 //routes handler
 const users = require('./routes/userAuth');
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
 const { Console } = require('console');
+const { options } = require('joi');
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/camp-my'
 
-mongoose.connect('mongodb://localhost:27017/camp-my');
+mongoose.connect(dbUrl)
 
 //error checking for db
 const db = mongoose.connection;
@@ -44,9 +53,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true })) //parse data
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+//security
+app.use(mongoSanitize());
 
+const secret = process.env.SECRET || 'abc';
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret,
+    }
+    
+});
+
+store.on('error', function (e) {
+    console.log('error', e);
+})
+//session cookie
 const sessionConfig = {
-    secret: 'abc',
+    store,
+    name: 'athena',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -59,6 +86,58 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net/",
+    "https://res.cloudinary.com/dv5vm4sqh/"
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/",
+    "https://res.cloudinary.com/dv5vm4sqh/"
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com",
+    "https://a.tiles.mapbox.com",
+    "https://b.tiles.mapbox.com",
+    "https://events.mapbox.com",
+
+];
+const fontSrcUrls = [];
+ 
+app.use(
+    helmet.contentSecurityPolicy({
+        directives : {
+            defaultSrc : [],
+            connectSrc : [ "'self'", ...connectSrcUrls ],
+            scriptSrc  : [ "'unsafe-inline'", "'self'", ...scriptSrcUrls ],
+            styleSrc   : [ "'self'", "'unsafe-inline'", ...styleSrcUrls ],
+            workerSrc  : [ "'self'", "blob:" ],
+            objectSrc  : [],
+            imgSrc     : [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/camp-my/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+                "https://images.unsplash.com/"
+            ],
+            fontSrc    : [ "'self'", ...fontSrcUrls ],
+            mediaSrc   : [ "https://res.cloudinary.com/dv5vm4sqh/" ],
+            childSrc   : [ "blob:" ]
+        }
+    })
+);
+
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -67,7 +146,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    console.log(req.session)
+    console.log(req.query);
     res.locals.signedUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
